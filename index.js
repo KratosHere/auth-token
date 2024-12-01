8c25df0f834156ecb79400ce5f9ccbddfbd0c475
shubhinux <KratosHere>
2024-12-01 11:16:38 +0530

    signin routing done

--- index.js
+++ index.js
@@ -33,6 +33,31 @@ app.post('/signup' , (req , res) => {
 })
 
 
+app.post("/signin" , (req , res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // LOGIC THAT IS THE USER REGIESTERED OR NOT AND IF REGISTERED SO SIGN IN
+
+    let foundUser = null;
+
+    for (let i = 0 ; i < USERS_DATABASE.length ; i++) {
+        if ((USERS_DATABASE[i].USERNAME === username) && (USERS_DATABASE[i].PASSWORD === password)) {
+            foundUser = USERS_DATABASE[i];
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            msg: `${foundUser.USERNAME} Successfully Logged IN`
+        })
+    }
+    else {
+        res.status(403).json({
+            msg:'User Not Found!'
+        })
+    }
+})
 
 
 app.get('/' , (req , res) => {res.json({msg:"HOMEPAGE!"})})
